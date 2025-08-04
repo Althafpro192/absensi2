94311f85ce2303be5410b375b5b7ea446ed7f0e7
Althafpro192 <althaf0907@gmail.com>
2025-08-04 15:13:46 +0700

    program baru

--- walikelas/ketidakhadiran_walkel.php
+++ walikelas/ketidakhadiran_walkel.php
@@ -68,7 +68,7 @@ $konek->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include "header.php"; ?>
+    <?php include "../header.php"; ?>
     <title>Data Ketidakhadiran</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
--- walikelas/tambahabsen_walkel.php
+++ walikelas/tambahabsen_walkel.php
@@ -67,7 +67,7 @@ if (isset($_POST['btnSimpan'])) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <?php include "header.php"; ?>
+    <?php include "../header.php"; ?>
 
     <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
     <script type="text/javascript">
